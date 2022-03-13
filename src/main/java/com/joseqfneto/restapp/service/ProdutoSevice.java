72fd58a3e27c9958e3df986cdea716d14391084a
José Neto <joseqfneto@gmail.com>
2022-03-12 23:16:16 -0300

    Implementação ProdutoService

--- src/main/java/com/joseqfneto/restapp/service/ProdutoSevice.java
+++ src/main/java/com/joseqfneto/restapp/service/ProdutoSevice.java
@@ -1,5 +1,9 @@
 package com.joseqfneto.restapp.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.joseqfneto.restapp.entity.Produto;
 import com.joseqfneto.restapp.repository.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,16 @@ public class ProdutoSevice {
     @Autowired
     ProdutoRepository produtoRepository;
 
-    
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public Produto save(Produto produto) {
+        return produtoRepository.save(produto);
+    }
 
 }
