563b4edc90e6b84ac09bb862bc95b467a4b5c989
José Neto <joseqfneto@gmail.com>
2022-03-12 23:20:04 -0300

    Implementação ProdutoController GET

--- src/main/java/com/joseqfneto/restapp/controller/ProdutoController.java
+++ src/main/java/com/joseqfneto/restapp/controller/ProdutoController.java
@@ -1,8 +1,22 @@
 package com.joseqfneto.restapp.controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import com.joseqfneto.restapp.dto.ProdutoDto;
+import com.joseqfneto.restapp.entity.Produto;
 import com.joseqfneto.restapp.service.ProdutoSevice;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +26,11 @@ public class ProdutoController {
 
     @Autowired
     ProdutoSevice produtoSevice;
+
+    @GetMapping
+    public ResponseEntity<Object> list() {
+        return ResponseEntity.status(HttpStatus.OK).body(produtoSevice.findAll());
+    }
 
     
 }
