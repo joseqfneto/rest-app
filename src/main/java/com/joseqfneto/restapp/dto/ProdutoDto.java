280cd90aba831aae0b0cdc55371472c61d6da9b6
José Neto <joseqfneto@gmail.com>
2022-03-12 23:12:11 -0300

    Criação DTO Produto

--- src/main/java/com/joseqfneto/restapp/dto/ProdutoDto.java
+++ src/main/java/com/joseqfneto/restapp/dto/ProdutoDto.java
@@ -1,5 +1,22 @@
 package com.joseqfneto.restapp.dto;
 
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProdutoDto {
-   
+    @NotBlank
+    private String nome;
+
+    @NotNull
+    private int quantidade;
+
+    @NotNull
+    private BigDecimal preco;
 }
